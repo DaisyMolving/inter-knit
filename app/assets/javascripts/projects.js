393ecd09d25867341ffb0600b6aefb379df03cd0
Daisy Molving <daisymolving@gmail.com>
2015-07-14 14:22:31 +0100

    added nav bar to show page

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -14,19 +14,6 @@ function projectOverview() {
   });
 }
 
-function howToVideo() {
-  $("#howToVideo").dialog({
-    dialogClass: "no-close",
-    height: 500,
-    width: 500,
-    modal: true,
-    autoOpen: false,
-    hide: {
-        effect: "fadeOut",
-        duration: 1000
-    }
-  });
-}
 
 function showSidebar() {
   $(".navbar-header").toggle(function() {
@@ -34,22 +21,41 @@ function showSidebar() {
   }) 
 }
 
-function hideSidebar() {
+function nextInstructions() {
   $("#next-stage").on("click", function(){
     console.log("clicked!");
     $.when($(".navbar-header").toggle(function() {
       $(".navbar-header").hide(1000);
     })).done(function(){
       $(".navbar-header").show(900);
+      $("#go-back").show();
     });
   });   
+};
+
+function previousInstructions() {
+  $("#go-back").on("click", function() {
+    console.log ("go back");
+    $.when($(".navbar-header").toggle(function() {
+      $(".navbar-header").hide(1000);
+    })).done(function() {
+      $(".navbar-header").show(900);
+    });
+  });
 };
 
 
 $(document).ready(function(){
   console.log("document ready");
-  hideSidebar();
+  $("#go-back").hide();
+
   projectOverview();
+
+  nextInstructions();
+
+  previousInstructions();
+
+  
   $("body").on("click", '.closeModal',function(){
     console.log("click to close");
     $("#projectOverview").dialog( "close" );
