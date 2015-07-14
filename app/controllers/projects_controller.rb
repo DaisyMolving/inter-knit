37544a24aecf1a566ecf00966e28383d8d857550
Daisy Molving <daisymolving@gmail.com>
2015-07-14 11:27:25 +0100

    created instance variables for instructions and stages related to project_id and appended them to page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,8 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @projects = Project.all
-    @project_stage = Stage.where(project_id: @project.id) 
+    @project_stage = Stage.where(project_id: @project.id)
+    @project_stage_instructions = Instruction.where(stage_id: @project_stage.first.id)
   end
 
 end
